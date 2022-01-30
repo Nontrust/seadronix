030864f60fc58f2f978c5855afeaedf52d7068ac
LEE <deed1515@naver.com>
2022-01-30 18:57:34 +0900

    Third commit  - Get Marker & Map

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -1,11 +1,24 @@
-function selectorByName(docName){
-    
-}
+<script>
+    function selectorByName(docName){
+        return document.getElementsByClassName(docName)[0];
+    }
 
-function selectorByClass(docClass){
+    function selectorByClass(docClass, index){
+        return document.getElementsByClassName(docClass)[0];
+    }
 
-}
+    function selectorById(docId, index){
+        return document.getElementById(docId);
+    }
+    var $ = function ( elem ) {
+	    var dom = document.querySelectorAll( elem ) , rtnVal = null ; 
+        if ( dom.length == 0 ) rtnVal = undefined ;
+        if ( dom.length == 1 ) rtnVal = dom[0] ; 
+        if ( dom.length > 1 ) rtnVal = dom ; 
+    	return rtnVal ; 
+    } ; 
+    function getInputValue(e) {
+        return e.value;
+    }
 
-function selectorById(docId){
-
-}
+</script>
--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -1,14 +1,35 @@
-<script type="text/javascript" src="//dapi.kakao.com/v2/maps/sdk.js?appkey=<%= javascript_Key %>" ></script>
 <script>
+    let xPos = 37.498362123762;
+    let yPos = 127.03451238865;
+
+    let markerXPos = 37.498362123762;
+    let markerYPos = 127.03451238865;
+
+    let zoomLevel = 3;
     const mapContainer = document.getElementById('map'), // 지도를 표시할 div 
         mapOption = { 
-            center: new kakao.maps.LatLng(33.450701, 126.570667),// 지도의 중심좌표
-            level: 3 // 지도의 확대 레벨
+            center: new kakao.maps.LatLng(
+                xPos, yPos
+                ),// 지도의 중심좌표
+            level: zoomLevel // 지도의 확대 레벨
         };
+
+    
+
+    let markerPos = new kakao.maps.LatLng(
+        markerXPos, markerYPos 
+    );
+
+    let marker = new kakao.maps.Marker({
+        position : markerPos,
+    });
+
+    function posRemote(value) {
+        mapContainer.mapOption.center = this.value;
+    };
 
     // 지도를 표시할 div와  지도 옵션으로  지도를 생성합니다
     const map = new kakao.maps.Map(mapContainer, mapOption); 
-
-    
+    marker.setMap(map);
 
 </script>
